c714107e4bf3f95f45d55239b9f096bd0a898d80
rubenrr29 <113448400+rubenrr29@users.noreply.github.com>
2022-11-22 15:44:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+
+"""
+this is code is written un python 3.8
+interpreted by the console
+"""
+print ("Hello World")
+print (50)
+print (3.10)
+print (50, "letters", 3.10)
+print ("hello", end =" " )
+print ("world")
